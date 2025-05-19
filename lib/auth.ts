885f7840f1b027a0daaa0d1d5f7b8450d5fbe8a9
louiskrisdy <louiskrisdy@gmail.com>
2025-05-19 13:59:08 +0700

    fixing oauth

--- lib/auth.ts
+++ lib/auth.ts
@@ -64,7 +64,7 @@ export const auth = betterAuth({
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID! as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET! as string,
-      redirectURI: process.env.BASE_URL + "/api/auth/callback/google",
+      redirectURI: process.env.BETTER_AUTH_URL + "/api/auth/callback/google",
     },
   },
   // emailAndPassword: {
